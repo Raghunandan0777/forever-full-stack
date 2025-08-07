0c88f3a538685b83cfdd59213c9e812df1bb3482
Raghunandan Shah <Raghunandanshah908014@gmail.com>
2025-08-07 20:44:23 +0530

    change in .env file

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -10,7 +10,7 @@ const ShopContextProvider = (props) => {
   const delivery_fee = 10;
   
   
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
   const [products, setProducts] = useState([]);
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
