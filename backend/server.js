3cfaaf3e8aca573c23760912262ef5d3b75f09a6
Raghunandan Shah <Raghunandanshah908014@gmail.com>
2025-08-07 17:12:43 +0530

    change in server

--- backend/server.js
+++ backend/server.js
@@ -19,11 +19,16 @@ connectDB()
 connectCloudinary()
 
 // widdlewares
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: ["http://localhost:5174", "http://localhost:5175", "http://localhost:5177", "http://localhost:5176"],
+  origin: ["http://localhost:5174",
+    "http://localhost:5175", "http://localhost:5177",
+    "http://localhost:5176",
+    " https://forever-frontend-emjsdylbd-raghunandan-shahs-projects.vercel.app",
+    "https://forever-admin-seven-tau.vercel.app"
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
@@ -46,7 +51,7 @@ app.get('/', (req, res) => {
 
 // api endpoint
 app.use("/api/user", userRoute)
-app.use("/api/product",productRouter)
+app.use("/api/product", productRouter)
 app.use('/api/order', orderRouter)
 app.use("/api/cart", cartRouter)
 
