ba4c59f8863488d03f297e192cb575855dc71c72
Raghunandan Shah <Raghunandanshah908014@gmail.com>
2025-08-08 16:07:05 +0530

    server change

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
-console.log('RAZORPAY_KEY_ID:', process.env.RAZORPAY_KEY_ID);
-console.log('RAZORPAY_KEY_SECRET:', process.env.RAZORPAY_KEY_SECRET);
+// console.log('RAZORPAY_KEY_ID:', process.env.RAZORPAY_KEY_ID);
+// console.log('RAZORPAY_KEY_SECRET:', process.env.RAZORPAY_KEY_SECRET);
 
 import express from "express";
 import cors from "cors";
